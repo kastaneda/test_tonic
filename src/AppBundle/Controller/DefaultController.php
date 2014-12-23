269bce3c9d0b515e2d5de5c75f0cbb4fd4f8187f
Dmitry Kolesnikov <kastaneda@gmail.com>
2014-12-23 08:00:23 +0200

    Global refCode matching route

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,13 @@ class DefaultController extends Controller
      */
     public function indexAction() {
         return $this->render('AppBundle::index.html.twig');
+    }
+
+    public function refCodeAction(Request $request) {
+        return $this->render('AppBundle::refcode.html.twig', [
+        'refcode' => $request->query->get('refcode'),
+        'redirectNext' => $request->attributes->get('redirectNext'),
+        ]);
     }
 
 }
