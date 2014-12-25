16f5f5f2d547b1d8bf86da6a08d013f2cba78042
Dmitry Kolesnikov <kastaneda@gmail.com>
2014-12-25 12:24:42 +0200

    Скрипт автоматической сборки

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# crontab:
+# */10  *  *  *  *  /home/gray/build/tonic_test/build.sh
+
+# webhook:
+# <?php echo touch('/home/gray/build/tonic_test/_build') ? 'OK' : 'Error';
+
+FLAG_START=_build
+FLAG_LOCK=_build_lock
+
+cd `dirname $0`
+
+if [ -f $FLAG_START ]
+then
+  if [ ! -f $FLAG_LOCK ]
+  then
+    touch $FLAG_LOCK
+    rm $FLAG_START
+    make && rm $FLAG_LOCK
+  fi
+fi
