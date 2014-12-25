2fc44b7718a8e28155e9b4c2486b2ffd3432ed70
Dmitry Kolesnikov <kastaneda@gmail.com>
2014-12-25 08:35:21 +0200

    Форма логина в navbar'е (fix #3)

---
+++ src/DK/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DK\UserBundle\Controller;
+
+use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+class SecurityController extends BaseController
+{
+
+    /**
+     * For embedding from layout template
+     */
+    public function navbarLoginAction(Request $request) {
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+        $csrfToken = $this->container->has('form.csrf_provider') ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate') : null;
+
+        return $this->container->get('templating')->renderResponse('UserBundle::navbar-login.html.twig', [
+                    'last_username' => $lastUsername,
+                    'csrf_token' => $csrfToken,
+        ]);
+    }
+
+}
